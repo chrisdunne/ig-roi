e57889ee2ad707f6e0be725d48b87b3e93b45305
chris <contact@chrisdunne.net>
2020-05-11 00:58:33 +0100

    get roi

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = function GetROI(props) {
+    const influencerEngagment = props.engagement;
+    const influencerFollowers = props.followers;
+    const conversionRate = props.conversion;
+    const averageOrderValue = props.aov;
+    const averageProductCost = props.cost;
+    const proposedPayment = props.payment;
+
+    let activeFollowers = ((influencerFollowers / influencerEngagment) * 100).toFixed(2);
+    let potentialSales = ((activeFollowers / conversionRate) * 100).toFixed(2);
+    let potentialRevenue = (potentialSales * averageOrderValue).toFixed(2);
+    let predictedProductCost = (averageProductCost * potentialRevenue).toFixed(2);
+    let predictedSales = potentialRevenue - predictedProductCost;
+
+    return predictedSales - proposedPayment;
+}
