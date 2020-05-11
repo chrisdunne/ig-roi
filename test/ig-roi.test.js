d20be9a243fc472437c7138d7e001335fed28d1a
chris <contact@chrisdunne.net>
2020-05-11 01:12:19 +0100

    added first test

---
+++ test/ig-roi.test.js
@@ -0,0 +1,14 @@
+const roi = require("../index.js");
+
+describe('check response is accurate', () => {
+    it('should return 3.5', () => {
+        roi.GetROI({
+            "engagement" : 1.5,
+            "followers" : 1000,
+            "conversion" : 3,
+            "aov" : 80,
+            "cost" : 50,
+            "payment" : 10
+        }, 3.5);
+    });
+});
